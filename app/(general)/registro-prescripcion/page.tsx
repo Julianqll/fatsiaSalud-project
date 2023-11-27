7cc61f491007c7f91fc78657a05c8142e9371c97
Julian Quispe <julianmql@hotmail.com>
2023-11-26 22:32:53 -0500

    fix: reference link fixed

--- app/(general)/registro-prescripcion/page.tsx
+++ app/(general)/registro-prescripcion/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
-import FormularioRegistroPrescripcion from "../../../components/FormularioRegistro/FormularioRegistro";
+import FormularioRegistroPrescripcion from "../../../components/FormularioRegistroPrescripcion/FormularioRegistroPrescripcion";
 
 function RegistroPrescripcionPage() {
     const {data : session} = useSession();
